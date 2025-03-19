06988fc6a7a081fcac4ccf44638b01b7e53e1466
K.Udul Dewmina <165687248+KUDewmina@users.noreply.github.com>
2025-03-20 00:44:48 +0530

    Update source_1.py

--- source_1.py
+++ source_1.py
@@ -1,4 +1,4 @@
-# First I coded this and it passes 986/987 testcases.Because input in first testcase is very long.Due to high complexity of this code ,time limit exceeded
+# First I coded this and it passes 986/987 testcases.Because input in failed testcase is very long.Due to high complexity of this code ,time limit exceeded
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
